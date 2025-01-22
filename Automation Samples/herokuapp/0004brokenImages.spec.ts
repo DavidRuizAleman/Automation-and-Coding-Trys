b20c88a8a7c94d1b163f0e2a108cb34775dd232d
DavidRuizAleman <41750793+DavidRuizAleman@users.noreply.github.com>
2025-01-22 10:36:15 -0600

    removed broken images
    file not ready, still working on ut, uploaded by mistake

--- Automation Samples/herokuapp/0004brokenImages.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('its alive', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("The Internet");
-});
