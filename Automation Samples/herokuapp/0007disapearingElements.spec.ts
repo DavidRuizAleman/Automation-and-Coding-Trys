e786aa85b37a92d69d7f7c80b9fc281995b9d947
DavidRuizAleman <41750793+DavidRuizAleman@users.noreply.github.com>
2025-01-26 20:27:00 -0600

    Works but it is ugly.
    Soft assertion make test fail.
    
    .isvisible is not working for ifs. all retur is vissible.,
    
    am tired will look again tomorrow

--- Automation Samples/herokuapp/0007disapearingElements.spec.ts
+++ Automation Samples/herokuapp/0007disapearingElements.spec.ts
@@ -16,17 +16,33 @@ test('Disappearing Elements', async ({ page }) => {
   await page.getByRole('link', { name: 'Disappearing Elements' }).click();
   await expect(page.getByRole('heading', { name: 'Disappearing Elements'})).toBeVisible();
   
-  let links = [  'home' ,  'about',   'contact us',   'gallery',   'portfolio', 'cloud']
-  let num=0
+  let links = [  'home',  'about',   'contact us',   'gallery',   'portfolio']  
   
+	
 	links.forEach(function (value) {
+		console.log("trying: "  + value);
+		const current = page.getByRole('link', { name: value});
 		try{
-			page.getByRole('link', { name: value}).toBeVisible()
-				num++;
+			expect.soft(page.getByRole('link',{ name: value})).toBeVisible();
 		}
-		catch(e){
+		catch{
 			console.log("Element not found: "  + value);
+			console.warn('Could not click on #may-not-exist, test will continue:', error);
 		}
+		
+		 //if (await ) {
+			 
+		 
+		
+		if(page.getByRole('link', { name: value}).isVisible()){
+			  page.waitForTimeout(3);
+			console.log("Element found: "  + value);
+		}
+		else{
+			  page.waitForTimeout(30);
+			console.log("Element not found: "  + value);
+			} 
+			
 		
 
 		
